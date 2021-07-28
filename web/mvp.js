4ca965f3e27d7b09e482a9d7febbd31fb467b45d
Terrillo Walls <terrillo@terrillo.com>
2021-07-28 17:05:58 -0500

    MVP blackjac

--- web/mvp.js
+++ web/mvp.js
@@ -1,5 +1,7 @@
 // 1. new-game
 // 2. dealing
+// 3. player-turn
+// 4. end-game
 
 var app = new Vue({
 	el: "#app",
@@ -14,6 +16,33 @@ var app = new Vue({
 	},
 	created: function () {
 		this.buld_deck();
+	},
+	watch: {
+		deck: function (old_value, new_value) {
+			var player_total = this.get_total(this.hands.player);
+			var dealer_total = this.get_total(this.hands.dealer);
+
+			if (player_total > 21) {
+				this.message = "Player BUST!";
+				this.state = "end-game";
+			}
+
+			if (dealer_total > 21) {
+				this.message = "Dealer BUST!";
+				this.state = "end-game";
+			}
+
+			if (player_total == 21) {
+				this.message = "Player has Blackjack!";
+			}
+
+			if (dealer_total == 21) {
+				this.hands.delear.map((card) => {
+					card.flipped = true;
+				});
+				this.message = "Dealer has Blackjack!";
+			}
+		},
 	},
 	methods: {
 		buld_deck: function () {
@@ -35,6 +64,10 @@ var app = new Vue({
 			this.state = "dealing";
 			this.message = "Dealing cards";
 
+			// Return to Deck
+			this.deck = this.deck.concat(this.hands.dealer);
+			this.deck = this.deck.concat(this.hands.player);
+
 			// Dealer
 			this.hands.dealer = [];
 			this.hands.dealer = this.hands.dealer.concat(this.pull_cards(1, true));
@@ -43,6 +76,9 @@ var app = new Vue({
 			// Player
 			this.hands.player = [];
 			this.hands.player = this.hands.player.concat(this.pull_cards(2, true));
+
+			this.state = "player-turn";
+			this.message = "Your Move";
 		},
 		pull_cards: function (count, flipped) {
 			const drawn_cards = _.take(this.deck, count);
@@ -51,6 +87,68 @@ var app = new Vue({
 			});
 			this.deck = _.drop(this.deck, count);
 			return drawn_cards;
+		},
+		action_hit: function () {
+			this.hands.player = this.hands.player.concat(this.pull_cards(1, true));
+		},
+		action_stand: function () {
+			this.dealer_move();
+		},
+		dealer_move: function () {
+			if (this.state !== "end-game") {
+				this.hands.dealer.map((card) => {
+					card.flipped = true;
+				});
+				while (this.get_total(this.hands.dealer) < 17) {
+					this.hands.dealer = this.hands.dealer.concat(this.pull_cards(1, true));
+				}
+				this.find_winner();
+			}
+		},
+		find_winner: function () {
+			var player_total = this.get_total(this.hands.player);
+			var dealer_total = this.get_total(this.hands.dealer);
+
+			if (player_total == dealer_total) {
+				this.message = "DRAW";
+				return;
+			}
+
+			if (player_total > dealer_total) {
+				this.message = "Player WINS";
+				return;
+			}
+
+			if (dealer_total > player_total) {
+				this.message = "Dealer WINS";
+				return;
+			}
+		},
+		get_total(cards) {
+			var total_value = 0;
+			var have_ace = false;
+
+			cards.forEach((card) => {
+				if (card.value == "J" || card.value == "Q" || card.value == "K") {
+					total_value += 10;
+				} else {
+					if (card.value == "A") {
+						have_ace = true;
+					} else {
+						total_value += Number(card.value);
+					}
+				}
+			});
+
+			if (have_ace) {
+				if (total_value > 10) {
+					total_value += 1;
+				} else {
+					total_value += 11;
+				}
+			}
+
+			return total_value;
 		},
 	},
 });
