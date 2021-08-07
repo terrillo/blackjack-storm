afe8f48202dc489f2f4f62282d6068199dc9718c
Terrillo Walls <terrillo@terrillo.com>
2021-08-07 09:09:59 -0500

    JS compressing

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const compressor = require("node-minify");
+const watcher = require("node-watch");
 
 const app = express();
 const port = 3000;
@@ -7,4 +9,16 @@ app.use("/", express.static("web"));
 
 app.listen(port, () => {
 	console.log(`Blackjack Storm Ready PORT -> ${port}`);
+
+	// JS Compressor
+	watcher("web/src/js", { recursive: true }, function () {
+		compressor.minify({
+			compressor: "gcc",
+			input: "./web/src/js/**/*.js",
+			output: "./web/dist/js/blackjack.js",
+			callback: function () {
+				console.log("-> JS Compressed");
+			},
+		});
+	});
 });
