12e2fb69049f899bfa9d0f6ac1af8eb5f9136276
Terrillo Walls <terrillo@terrillo.com>
2021-08-12 21:10:03 +0000

    ETA router

--- server.js
+++ server.js
@@ -1,11 +1,28 @@
 const express = require("express");
 const compressor = require("node-minify");
 const watcher = require("node-watch");
+const eta = require("eta");
+const path = require("path");
 
 const app = express();
 const port = 3000;
 
-app.use("/", express.static("web"));
+app.engine("eta", eta.renderFile);
+app.set("view engine", "eta");
+app.set("views", "./web/views");
+eta.configure({
+	views: path.join(__dirname, "./web/views"),
+});
+
+app.use("/dist/", express.static("web/dist"));
+
+app.get("/", function (req, res) {
+	res.render("marketing");
+});
+
+app.get("/play-now", function (req, res) {
+	res.render("play-now");
+});
 
 app.listen(port, () => {
 	console.log(`Blackjack Storm Ready PORT -> ${port}`);
