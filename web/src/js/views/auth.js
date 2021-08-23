1b4e2b8f46a24fc58e137405e9ac13968bac4501
Terrillo Walls <terrillo@terrillo.com>
2021-08-23 22:03:41 +0000

    Play now styling

--- web/src/js/views/auth.js
+++ web/src/js/views/auth.js
@@ -1,52 +1,65 @@
-var viewAuth = new Vue({
-	el: "#view--auth",
-	data: {
-		username: "",
-		email: "",
-		password: "",
-		error: false,
-		view: false,
-		info: false,
-	},
-	created: function () {
-		firebase.auth().onAuthStateChanged((user) => {
-			this.info = user;
-		});
-	},
-	methods: {
-		modal: function (view) {
-			this.view = view;
+if (document.getElementById("view--auth") !== null) {
+	var viewAuth = new Vue({
+		el: "#view--auth",
+		data: {
+			username: "",
+			email: "",
+			password: "",
+			error: false,
+			view: false,
+			info: false,
 		},
-		log_in: function () {
-			firebase
-				.auth()
-				.signInWithEmailAndPassword(this.email, this.password)
-				.then((userCredential) => {
-					this.info = userCredential.user;
-					this.modal(false);
-				})
-				.catch((error) => {
-					this.error = error.message;
-				});
+		created: function () {
+			firebase.auth().onAuthStateChanged((user) => {
+				this.info = user;
+			});
 		},
-		sign_up: function () {
-			firebase
-				.auth()
-				.createUserWithEmailAndPassword(this.email, this.password)
-				.then((userCredential) => {
-					this.info = userCredential.user;
-					this.modal(false);
-					var user = firebase.auth().currentUser;
-					user.updateProfile({
-						displayName: this.username,
+		watch: {
+			info: function () {
+				if (this.info !== null && document.getElementById("view--gameboard") == null) {
+					location.href = "/play-now";
+				}
+
+				if (this.info == null && document.getElementById("view--gameboard") !== null) {
+					location.href = "/";
+				}
+			},
+		},
+		methods: {
+			modal: function (view) {
+				this.view = view;
+			},
+			log_in: function () {
+				firebase
+					.auth()
+					.signInWithEmailAndPassword(this.email, this.password)
+					.then((userCredential) => {
+						this.info = userCredential.user;
+						this.modal(false);
+					})
+					.catch((error) => {
+						this.error = error.message;
 					});
-				})
-				.catch((error) => {
-					this.error = error.message;
-				});
+			},
+			sign_up: function () {
+				firebase
+					.auth()
+					.createUserWithEmailAndPassword(this.email, this.password)
+					.then((userCredential) => {
+						this.info = userCredential.user;
+						this.modal(false);
+						var user = firebase.auth().currentUser;
+						user.updateProfile({
+							displayName: this.username,
+						});
+					})
+					.catch((error) => {
+						this.error = error.message;
+					});
+			},
+			sign_out: function () {
+				firebase.auth().signOut();
+			},
 		},
-		sign_out: function () {
-			firebase.auth().signOut();
-		},
-	},
-});
+	});
+}
